3abf835932f1c70501846fb27516ba7b5b4b7d30
mingchun.or <mingchun.or@harborx.com>
2016-02-27 19:24:10 +0800

    add doc deps and travis CI

--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule Exbackoff.Mixfile do
   use Mix.Project
 
+  @version "0.0.2"
+
   def project do
     [app: :exbackoff,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.2",
+     description: description,
+     package: package,
+     source_url: "https://github.com/mingchuno/exbackoff",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     docs: [extras: ["README.md"], main: "readme",
+              source_ref: "v#{@version}",
+              source_url: "https://github.com/mingchuno/exbackoff"]
+    ]
+  end
+
+  defp description do
+    """
+    Simple exponential backoffs in Elixir.
+    """
   end
 
   # Configuration for the OTP application
@@ -29,7 +44,17 @@ defmodule Exbackoff.Mixfile do
   defp deps do
     [
       {:excheck, "~> 0.3.2", only: :test},
+      {:earmark, "~> 0.1", only: :docs},
+      {:ex_doc, "~> 0.10", only: :docs},
+      {:inch_ex, "~> 0.4", only: :docs},
       {:triq, github: "krestenkrab/triq", only: :test}
     ]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["MC Or"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mingchuno/exbackoff"}]
   end
 end
