608f463d29dc643587e40cf8086987e480a60363
mingchun.or <mingchuno@gmail.com>
2019-02-06 23:10:24 +0800

    fix travis build script

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Exbackoff.MixProject do
     [
       app: :exbackoff,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
